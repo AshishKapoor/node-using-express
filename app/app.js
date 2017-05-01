1d426294d436229fc28785280d7a47b7b9ae18ee
Ashish Kapoor <ashish.kapoor@navitas.com>
2017-05-01 19:58:00 +0530

    using express

--- app/app.js
+++ app/app.js
@@ -2,9 +2,24 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<h2>Hello World</h2>')
 })
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('app listening on port 3000!')
-})
+});
+
+
+
+// SIMPLE SERVER
+// var http = require('http')
+
+// var myServer = http.createServer(function (req, res) {
+//   res.writeHead(200, {"Content-Type": "text/html"});
+//   res.write("<h1>Roux Meetup</h1>");
+//   res.end();
+// })
+
+
+// myServer.listen(3000);
+// console.log("Go to localhost:3000 of your browser.");
