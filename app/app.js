7b92627b15ee5ff5c62712fa916e99e8196a7a05
Ashish Kapoor <ashish.kapoor@navitas.com>
2017-05-15 10:07:15 +0530

    fun with routing...

--- app/app.js
+++ app/app.js
@@ -1,10 +1,41 @@
 var express = require('express')
 var app = express()
+var dataFile = require('./data/data.json')
 
-app.get('/', function (req, res) {
-  res.send('<h2>Swift Developers</h2>')
+app.set('port', process.env.PORT || 3000)
+
+app.get('/', function (req, res) { // root route
+  res.send(`
+    <h1>Welcome</h1>
+    <p>Swift Meetups</p>
+  `)
 })
 
-var server = app.listen(3000, function () {
-  console.log('app listening on port 3000!')
-});
+app.get('/speakers', function (req, res) { // root/speakers route
+  var info = ''
+  dataFile.speakers.forEach(function(item) { // loop through the speakers
+    info += (`
+      <li> 
+        <h2>${item.name}</h2>
+        <p>${item.summary}</p>
+      </li> 
+      `)
+  })
+  res.send(` 
+    <h1>Swift Meetups</h1>
+    ${info}
+  `)
+})
+
+app.get('/speakers/:speakerid', function (req, res) { // root/speakers/<speakerid> route
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+    <h1>${speaker.title}</h1>
+    <h2>with ${speaker.name}</h2>
+    <p>${speaker.summary}</p>
+  `)
+})
+
+var server = app.listen(app.get('port'), function () {
+  console.log('app listening on port ' + app.get('port'))
+})
