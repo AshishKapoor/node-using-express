47c2105685c60d29eb49a8f7159b6aecaab7d448
Ashish Kapoor <ashish.kapoor@navitas.com>
2017-05-15 16:09:28 +0530

    added correct view files

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res) { // root route
-  res.send(`
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <h1>Welcome</h1>
-    <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
-    <p>Swift Meetups</p>
-    <script src="/reload/reload.js"></script>
-  `)
+  res.render('index')
 })
 
 module.exports = router
