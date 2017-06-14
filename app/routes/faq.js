85151a9995839198e92ac9074a9fe15a099b9479
Ashish Kapoor <swiftobjc@gmail.com>
2017-06-14 16:30:36 +0530

    placed navbar to the top of header

--- app/routes/faq.js
+++ app/routes/faq.js
@@ -2,10 +2,8 @@ var express = require('express')
 var router = express.Router();
 
 router.get('/faq', function (req, res) { // root/speakers route
-  var info = ''
   res.send(` 
-    <h1>Swift Meetups</h1>
-    ${info}
+    <h1>Frequently asked questions</h1>
   `)
 })
 
--- app/routes/tutorials.js
+++ app/routes/tutorials.js
@@ -2,22 +2,10 @@ var express = require('express')
 var router = express.Router();
 
 router.get('/tutorials', function (req, res) { // root/speakers route
-  var info = ''
-  var dataFile = req.app.get('appData')
-
-  dataFile.speakers.forEach(function(item) { // loop through the speakers
-    info += (`
-      <link rel="stylesheet" type="text/css" href="/css/style.css">
-      <li> 
-        <h2>${item.name}</h2>
-        <img src="/images/speakers/${item.shortname}_tn.jpg" alt="${item.name}">
-        <p>${item.summary}</p>
-      </li> 
-      `)
-  })
   res.send(` 
-    <h1>Swift Meetups</h1>
-    ${info}
+    <p>${req}</p>
+    <p>${res}</p>
+    <a href="https://www.youtube.com"> Youtube.com </a>
   `)
 })
 
